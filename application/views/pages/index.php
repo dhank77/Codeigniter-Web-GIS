3932991e268762ddce44500f725278741350bd3a
Hamdani <d41113512@gmail.com>
2020-01-06 17:05:54 +0800

    fixed index pages

--- application/views/pages/index.php
+++ application/views/pages/index.php
@@ -1,18 +1,53 @@
 <body>
 	<div class="preloader">
-        <div class="lds-ripple">
-            <div class="lds-pos"></div>
-            <div class="lds-pos"></div>
-        </div>
-    </div>
-    <!-- ============================================================== -->
-    <!-- Main wrapper - style you can find in pages.scss -->
-    <!-- ============================================================== -->
-    <div id="main-wrapper" data-layout="vertical" data-navbarbg="skin5" data-sidebartype="full" data-sidebar-position="absolute" data-header-position="absolute" data-boxed-layout="full">
-        <!-- ============================================================== -->
-        <!-- Topbar header - style you can find in pages.scss -->
-        <!-- ============================================================== -->
-        <?php $this->load->view('layouts/topbar') ?>
+		<div class="lds-ripple">
+			<div class="lds-pos"></div>
+			<div class="lds-pos"></div>
+		</div>
+	</div>
+	<!-- ============================================================== -->
+	<!-- Main wrapper - style you can find in pages.scss -->
+	<!-- ============================================================== -->
+	<div id="main-wrapper" data-layout="vertical" data-navbarbg="skin5" data-sidebartype="full" data-sidebar-position="absolute" data-header-position="absolute" data-boxed-layout="full">
+		<!-- ============================================================== -->
+		<!-- Topbar header - style you can find in pages.scss -->
+		<!-- ============================================================== -->
+		<header class="topbar" data-navbarbg="skin5">
+			<nav class="navbar top-navbar navbar-expand-md navbar-dark">
+				<div class="navbar-header" data-logobg="skin5">
+					<!-- ============================================================== -->
+					<!-- Logo -->
+					<!-- ============================================================== -->
+					<a class="navbar-brand" href="index.html">
+						<!-- Logo icon -->
+						<b class="logo-icon">
+							<!--You can put here icon as well // <i class="wi wi-sunset"></i> //-->
+							<!-- Dark Logo icon -->
+							<img src="<?= base_url('vendor/xtreme/') ?>assets/images/logo-icon.png" alt="homepage" class="dark-logo" />
+							<!-- Light Logo icon -->
+							<img src="<?= base_url('vendor/xtreme/') ?>assets/images/logo-light-icon.png" alt="homepage" class="light-logo" />
+						</b>
+						<!--End Logo icon -->
+						<!-- Logo text -->
+						<span class="logo-text">
+							<!-- dark Logo text -->
+							<div class="mt-3">
+								<h3>Geographic Information System</h3>
+							</div>
+						</span>
+					</a>
+					<!-- ============================================================== -->
+					<!-- End Logo -->
+					<!-- ============================================================== -->
+				</div>
+				<!-- ============================================================== -->
+				<!-- End Logo -->
+				<!-- ============================================================== -->
+			</nav>
+		</header>
 		<?php echo $map['html']; ?>
-    </div>
+		<footer class="footer text-center">
+			All Rights Reserved by Xtreme Admin. Designed and Developed by <a href="https://wrappixel.com">WrapPixel</a>.
+		</footer>
+	</div>
 </body>
