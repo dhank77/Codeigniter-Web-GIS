75d847c3091df5b13d501a24c71ca38349715c68
Hamdani <d41113512@gmail.com>
2020-01-06 21:13:49 +0800

    add some marker

--- application/controllers/Pages.php
+++ application/controllers/Pages.php
@@ -1,19 +1,25 @@
 <?php
-defined('BASEPATH') OR exit('No direct script access allowed');
+defined('BASEPATH') or exit('No direct script access allowed');
 
-class Pages extends CI_Controller {
+class Pages extends CI_Controller
+{
 
 	public function index()
 	{
 		$this->load->library('googlemaps');
 
-		$config['center'] = '37.4419, -122.1419';
+		$config['center'] = '-2.668611, 118.862222';
 		$config['zoom'] = 'auto';
 		$this->googlemaps->initialize($config);
 
-		$marker = array();
-		$marker['position'] = '37.429, -122.1419';
-		$this->googlemaps->add_marker($marker);
+		$data = $this->db->get('marker')->result_array();
+		foreach($data as $d){
+			$marker = array();
+			$marker['position'] = $d['latlong'];
+			$marker['infowindow_content'] = $d['keterangan'];
+			$marker['icon'] = $d['icon'];
+			$this->googlemaps->add_marker($marker);
+		}
 		$data['map'] = $this->googlemaps->create_map();
 
 		$this->load->view('layouts/header', $data);
--- application/libraries/Googlemaps.php
+++ application/libraries/Googlemaps.php
@@ -20,10 +20,10 @@ class Googlemaps {
 	var $adsenseFormat				= 'HALF_BANNER';			// The format of the AdUnit
 	var $adsensePosition			= 'TOP_CENTER';				// The position of the AdUnit
 	var $adsensePublisherID			= '';						// Your Google AdSense publisher ID
-	var $apiKey						= ''; 						// If you've got an API key you can use it by passing this parameter. Setup an API key here: https://code.google.com/apis/console
+	var $apiKey						= 'AIzaSyAeC4ntC2uJJeQAx7PkbYqMm7TGnzK6hX4'; 						// If you've got an API key you can use it by passing this parameter. Setup an API key here: https://code.google.com/apis/console
 	var $backgroundColor			= '';						// A hex color value shown as the map background when tiles have not yet loaded as the user pans
 	var $bicyclingOverlay			= FALSE;					// If set to TRUE will overlay bicycling information (ie. bike paths and suggested routes) onto the map by default 
-	var $center						= "37.4419, -122.1419";		// Sets the default center location (lat/long co-ordinate or address) of the map. If defaulting to the users location set to "auto"
+	var $center						= "-2.668611, 118.862222";		// Sets the default center location (lat/long co-ordinate or address) of the map. If defaulting to the users location set to "auto"
 	var $class 						= '';						// A class name if wishing to style the map further through CSS. Can also be useful if wanting it to be responsive etc.
 	var $cluster					= FALSE;					// Whether to cluster markers
 	var $clusterGridSize			= 60;						// The grid size of a cluster in pixels
@@ -105,7 +105,7 @@ class Googlemaps {
 	var	$tilt						= 0;						// The angle of tilt. Currently only supports the values 0 and 45 in SATELLITE and HYBRID map types and at certain zoom levels
 	var	$trafficOverlay				= FALSE;					// If set to TRUE will overlay traffic information onto the map by default 
 	var	$version					= "3";						// Version of the API being used. Not currently used in the library
-	var $zoom						= 13;						// The default zoom level of the map. If set to "auto" will autozoom/center to fit in all visible markers. If "auto", also overrides the $center parameter
+	var $zoom						= 8;						// The default zoom level of the map. If set to "auto" will autozoom/center to fit in all visible markers. If "auto", also overrides the $center parameter
 	var $zoomControlPosition		= '';						// The position of the Zoom control, eg. 'BOTTOM_RIGHT'
 	var $zoomControlStyle			= '';						// The size of the zoom control. blank, 'SMALL' or 'LARGE' values accepted.
 	
